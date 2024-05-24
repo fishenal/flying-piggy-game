496f21074abcd5f392fbf65c5cc76aeb22320f85
fishenal <yu_dong_han@hotmail.com>
2024-05-24 13:01:24 +0800

    resize

--- src/main.ts
+++ src/main.ts
@@ -16,31 +16,40 @@ export const app = new Application();
 // globalThis.__PIXI_APP__ = app;
 /** Set up a resize function for the app */
 function resize() {
-    // const windowWidth = window.innerWidth;
-    // const windowHeight = window.innerHeight;
-    // const minWidth = 900;
-    // const minHeight = 700;
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const minWidth = 1024;
+    const minHeight = 768;
 
     // Calculate renderer and canvas sizes based on current dimensions
-    // const scaleX = windowWidth < minWidth ? windowWidth / minWidth : 1;
-    // const scaleY = windowHeight < minHeight ? windowHeight / minHeight : 1;
-    // const scale = Math.min(scaleX, scaleY);
-    // app.stage.scale = scale;
-    emitter.emit('onResize', { width: window.innerWidth, height: window.innerHeight });
+    const scaleX = windowWidth < minWidth ? minWidth / windowWidth : 1;
+    const scaleY = windowHeight < minHeight ? minHeight / windowHeight : 1;
+    const scale = scaleX > scaleY ? scaleX : scaleY;
+    const width = windowWidth * scale;
+    const height = windowHeight * scale;
+
+    // Update canvas style dimensions and scroll window up to avoid issues on mobile resize
+    app.renderer.canvas.style.width = `${windowWidth}px`;
+    app.renderer.canvas.style.height = `${windowHeight}px`;
+    window.scrollTo(0, 0);
+
+    // Update renderer  and navigation screens dimensions
+    app.renderer.resize(width, height);
+
+    emitter.emit('onResize', { width: width, height: height });
 }
 
 /** Setup app and initialise assets */
 async function init() {
     // Initialize app
     await app.init({
-        resolution: window.devicePixelRatio,
+        resolution: Math.max(window.devicePixelRatio, 2),
         backgroundColor: 0xffffff,
         autoDensity: true,
-        resizeTo: window,
     });
 
     window.addEventListener('resize', resize);
-
+    resize();
     // Add pixi canvas element (app.canvas) to the document's body
     document.body.appendChild(app.canvas);
 
